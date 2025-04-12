29238bf8b1038537ff8cbdbe0ea8a91d771b999e
Simon Howard <fraggle@soulsphere.org>
2025-04-12 19:11:55 -0400

    Identify thing triggers as well

--- tools/list_triggers.py
+++ tools/list_triggers.py
@@ -21,6 +21,17 @@ def describe_line(line_idx, line):
         ACS_LINE_TYPES[line.action], line_idx, x, y,
     )
 
+def describe_thing(thing_idx, thing):
+    return "%s by thing %d at (%d, %d)" % (
+        ACS_LINE_TYPES[thing.action], thing_idx, thing.x, thing.y,
+    )
+
+def describe_trigger(idx, trigger):
+    if isinstance(trigger, ZThing):
+        return describe_thing(idx, trigger)
+    else:
+        return describe_line(idx, trigger)
+
 w = WAD()
 w.from_file(sys.argv[1])
 for name in w.maps:
@@ -31,10 +42,14 @@ for name in w.maps:
         if line.action in ACS_LINE_TYPES:
             scripts.setdefault(line.arg0, []).append((line_idx, line))
 
-    for script, lines in sorted(scripts.items()):
+    for thing_idx, thing in enumerate(ed.things):
+        if thing.action in ACS_LINE_TYPES:
+            scripts.setdefault(thing.arg0, []).append((thing_idx, thing))
+
+    for script, triggers in sorted(scripts.items()):
         print("\tScript %d:" % script)
-        for line_idx, line in lines:
-            print("\t\t" + describe_line(line_idx, line))
+        for idx, trigger in triggers:
+            print("\t\t" + describe_trigger(idx, trigger))
 
     if True:
         lines = []
@@ -45,8 +60,8 @@ for name in w.maps:
                 m = SCRIPT_DEF_RE.match(s)
                 if m:
                     script = int(m.group(1))
-                    for line_idx, line in scripts.get(script, []):
-                        lines.append("// " + describe_line(line_idx, line))
+                    for idx, trigger in scripts.get(script, []):
+                        lines.append("// " + describe_trigger(idx, trigger))
                 lines.append(s)
 
         with open(acs_filename, "w") as f:
